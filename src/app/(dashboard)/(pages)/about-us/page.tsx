cf7f2db66a1f0c3f46f836620be4d5a0307d167c
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2026-02-02 00:08:53 +0200

    Added magicui component

--- src/app/(dashboard)/(pages)/about-us/page.tsx
+++ src/app/(dashboard)/(pages)/about-us/page.tsx
@@ -2,7 +2,7 @@ import AboutPageView from "@/src/features/about/components/view/aboutpage-view";
 
 function AboutUsPage() {
   return (
-    <div>
+    <div className="bg-white">
       <AboutPageView />
     </div>
   );
