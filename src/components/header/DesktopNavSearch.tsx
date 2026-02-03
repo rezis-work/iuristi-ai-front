a91d92e4c91422601ce6d58cc3c1f562eacc9e59
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2026-02-03 11:00:01 +0200

    Updated header Search ichon cursor pounter  component

--- src/components/header/DesktopNavSearch.tsx
+++ src/components/header/DesktopNavSearch.tsx
@@ -10,7 +10,7 @@ export function DesktopNavSearch({ onClick }: DesktopNavSearchProps) {
   return (
     <button
       onClick={onClick}
-      className="hidden lg:flex items-center text-zinc-400 hover:text-white transition-colors py-2 pb-3"
+      className="hidden lg:flex items-center mt-1 text-zinc-400 hover:text-white transition-colors py-2 pb-3 cursor-pointer"
       aria-label="Open search"
     >
       <Search className="w-5 h-5" />
