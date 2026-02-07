c222414a544e98ed48efb9bde7c280e71d42f249
Rezi Karanadze <karanadzerevazi@gmail.com>
2026-02-07 16:51:31 +0400

    test deploy

--- src/app/giorgi/page.tsx
+++ src/app/giorgi/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
-  return (
-    <div>
-      giorgi
-    </div>
-  )
+  return <div>Iuristi platform production</div>;
 }
