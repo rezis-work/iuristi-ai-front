8d2f4a3555a9c626aef558b9973e23b4703527c8
nozadze22 <giorgin432@gmail.com>
2026-03-07 18:15:30 +0400

    add sugest text in search bar

--- src/features/AI/search/schema/search-schema.ts
+++ src/features/AI/search/schema/search-schema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const searchRequestSchema = z.object({
     query: z.string().min(1, "გთხოვ, შეიყვანე საძიებო სიტყვა"),
     scoreThreshold : z.number().min(0).max(1),
-    topK : z.number().int().positive().max(1),
+    topK : z.number().int().positive().max(100),
   });
   
   export type SearchRequest = z.infer<typeof searchRequestSchema>;
